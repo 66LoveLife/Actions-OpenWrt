279995cbee1972b4f83b06d6671330cef3609035
MorningStar <39751452+66LoveLife@users.noreply.github.com>
2019-12-10 09:07:42 -0600

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,7 +8,6 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/Ameykyl/luci-app-koolproxyR package/luci-app-koolproxyR
-#git clone https://github.com/Ameykyl/luci-app-ssr-plus-jo package/luci-app-ssr-plus-jo
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/rosywrt/luci-theme-rosy package/luci-theme-rosy
