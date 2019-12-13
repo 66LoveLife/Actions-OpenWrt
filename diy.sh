0a03027765cf4c5b0deb262ec723d561179a83bb
MorningStar <39751452+66LoveLife@users.noreply.github.com>
2019-12-13 13:01:04 -0600

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,6 +7,19 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# NTP
+echo "Settinng NTP Server"
+sed -i "s/0.openwrt.pool.ntp.org/ntp1.aliyun.com/g" ./package/base-files/files/bin/config_generate
+sed -i "s/1.openwrt.pool.ntp.org/ntp2.aliyun.com/g" ./package/base-files/files/bin/config_generate
+sed -i "s/2.openwrt.pool.ntp.org/ntp3.aliyun.com/g" ./package/base-files/files/bin/config_generate
+sed -i "s/3.openwrt.pool.ntp.org/ntp4.aliyun.com/g" ./package/base-files/files/bin/config_generate
+# Timezone
+echo "Settinng Timezone"
+sed -i "s/UTC/CST-8/g" ./package/base-files/files/bin/config_generate
+sed -i "/CST-8/a set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate 
+# set root password(default: password)
+echo "Set root password(default: password)"
+sed -i 's|root.*|root:$1$pFEtE/6h$4s9J0gQfhU9wnfjnSTH5m.:18243:0:99999:7:::|g' ./package/base-files/files/etc/shadow
 git clone https://github.com/Ameykyl/luci-app-koolproxyR package/luci-app-koolproxyR
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
